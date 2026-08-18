921ec5731055a488deb9a799bb4cbdd95a41a063
Viktor Gabriel <gabryoliver22@gmail.com>
2026-08-17 23:32:41 -0300

    fix: define porta padrao 3000 quando PORT nao esta definida no ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,39 @@
-console.log("hello world");
+import express, { Request, Response } from "express";
+import { inMemoryTransactionRepository } from "./repositories/in-memory-transaction-repository";
+import { CreateTransactionDTO, Transaction } from "./domain/transaction"
+import GetSummaryUseCase from "./use-cases/get-summary"
+import { CreateTransactionUseCase } from "./use-cases/create-transaction";
+
+const app = express();
+app.use(express.json());
+
+const repository = new inMemoryTransactionRepository()
+
+const createTransactionUseCase = new CreateTransactionUseCase(repository)
+
+const getSummaryUseCase = new GetSummaryUseCase(repository)
+
+app.post('/transactions', (req: Request, res: Response) => {
+    try {
+        const transaction = createTransactionUseCase.execute(req.body)
+        return res.status(201).json(transaction)
+    } catch (error: any) {
+        return res.status(400).json({ message: error.message })
+    }
+});
+
+app.get('/summary', (req:Request, res:Response) => {
+    const summary = getSummaryUseCase.execute()
+    return res.status(200).json(summary)
+});
+
+app.get('/transactions', (req:Request, res:Response) => {
+    const transactions = repository.findAll()
+    return res.status(200).json(transactions)
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
